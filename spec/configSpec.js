bba2fa8a96b070c4d627a9e2edaec56df5befb9c
Rick Clark <rkingsleyclark@gmail.com>
2017-03-09 18:37:36 +0000

    Add first Jasmine test and console output reporter

---
+++ spec/configSpec.js
@@ -0,0 +1,2 @@
+const Reporter = require('jasmine-console-reporter');
+jasmine.getEnv().addReporter(new Reporter());
---
+++ spec/moveSpec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Move', function() {
+
+  it("contains spec with an expectation", function() {
+  expect(true).toBe(true);
+});
+
+});
